001856ba78d8f3c54a27499fa244491ea7f7eaf9
vencons <vencon.s@outlook.com>
2026-06-25 23:58:40 +0800

    自动同步：添加新功能

--- test/pth.py
+++ test/pth.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     # 当前子任务队列 = [1, 2, 3, 4]
     # random.shuffle(当前子任务队列)
     # print(当前子任务队列)
-    name1="归属a22。。孤影剑的跟班"
+    name1="129甲"
     xinx=解析玩家全名(name1)
     print(xinx)
     
